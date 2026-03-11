0b0ee7636916805e36f9f9254857ee7761d99154
Ryo <kanekuni0104@gmail.com>
2026-03-11 11:08:43 +0900

    feat: 空文字でタスクを追加しようとした時のテストを追加
    - テスト観点：空文字ではタスク登録できない
    - テスト観点：タスク登録しようとするとエラ〜メッセージが表示される

--- test/setting/widgets/register_task_test.dart
+++ test/setting/widgets/register_task_test.dart
@@ -24,5 +24,23 @@ void main() {
 
       expect(find.text(AppStrings.newTaskModalTitle), findsOneWidget);
     });
+
+    testWidgets('空文字では保存されずモーダルが閉じないこと', (WidgetTester tester) async {
+      // ウィジェットを記述する
+      await tester.pumpWidget(testElement);
+
+      // モーダルを開く
+      await tester.tap(find.text(AppStrings.registerTaskButton));
+      await tester.pumpAndSettle();
+
+      // タスク名なしで保存ボタン押下
+      await tester.tap(find.text(AppStrings.saveButton));
+      await tester.pumpAndSettle();
+
+      // テスト観点：モーダルタイトルが表示され続けること = モーダルが閉じていない
+      // テスト観点：エラーテキストが出ていること
+      expect(find.text(AppStrings.newTaskModalTitle), findsOneWidget);
+      expect(find.text(AppStrings.taskNameRequiredError), findsOneWidget);
+    });
   });
 }
